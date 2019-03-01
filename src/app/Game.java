5ec626ca32b6436fc96183b39d25f57034bb783a
TheIncgi <theincgi@gmail.com>
2019-03-01 14:01:49 -0500

    Updated title

--- src/app/Game.java
+++ src/app/Game.java
@@ -10,10 +10,10 @@ import javafx.stage.StageStyle;
 import resources.R;
 
 public class Game extends Application{
-
+	public static final String VERSION = "1.0.0";
 	public static final int SIZE = 1000;
 	private static Game instance;
-	public static final String TITLE = "Maze of Shadows";
+	public static final String TITLE = "[WIP Title] Maze of Shadows - Version "+VERSION;
 	
 	private Engine engine;
 	
