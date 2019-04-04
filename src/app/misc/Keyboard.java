6f8a561a208fad943bcb214ac713f47743d073e2
TheIncgi <theincgi@gmail.com>
2019-04-04 13:04:50 -0400

    Input

--- src/app/misc/Keyboard.java
+++ src/app/misc/Keyboard.java
@@ -1,5 +1,29 @@
 package app.misc;
 
-public class Keyboard {
+import java.util.HashMap;
 
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+
+/**
+ * Used to track which keys are currently down
+ * */
+public class Keyboard {
+	
+	private static final HashMap<KeyCode, Boolean> heldKeys = new HashMap<>();
+	private Keyboard() {}
+	
+	public static void onKeyPress(KeyEvent event) {
+		heldKeys.put(event.getCode(), true);
+	}
+	public static void onKeyRelease(KeyEvent event) {
+		heldKeys.remove(event.getCode());
+	}
+	
+	public static boolean isHeld( KeyCode code ) {
+		return heldKeys.getOrDefault(code, false);
+	}
+	public static boolean isHeld( Keybinding binding ) {
+		return isHeld( binding.getKeyCode() );
+	}
 }
