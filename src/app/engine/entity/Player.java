87f620731bbb209234b86b6ea5c139f339044e14
TheIncgi <theincgi@gmail.com>
2019-04-10 18:01:18 -0400

    Update Player

--- src/app/engine/entity/Player.java
+++ src/app/engine/entity/Player.java
@@ -4,12 +4,34 @@ import app.engine.tiles.Emissive;
 import app.misc.DoublePosition;
 import javafx.scene.paint.Color;
 
-public class Player extends LivingEntity {
+import java.util.HashMap;
 
+import app.engine.items.BaseItem;
+
+public class Player extends LivingEntity {
+	HashMap<BaseItem, Integer> inventory = new HashMap<>();
+	int gold = 0;
 	
+	public Player(int maxHealth) {
+		super(maxHealth);
+	}
 	
-	public Player() {
-		super( 5 ); //TODO what is the players max health, this is a placeholder
+	public void useItem(BaseItem item) {
+		
+	}
+	
+	public HashMap<BaseItem, Integer> getInventory() {
+		return inventory;
+	}
+	
+	public void addItemToInventory(BaseItem item) {}
+	
+	public int getGold() {
+		return gold;
+	}
+	
+	public void setGold(int gold) {
+		this.gold = gold;
 	}
 	
 	
